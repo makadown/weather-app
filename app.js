eee4d8d03a91f937fb4787b22fca4ca54ad21cdc
Mario Serrano <maseflores79@gmail.com>
2018-06-24 18:27:40 -0700

    explorando httprequest

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ request({
         json: true
     },
     (error, response, body) => {
-        console.log(JSON.stringify(body, undefined, 2));
+        // console.log(JSON.stringify(body, undefined, 2));
+        // console.log(JSON.stringify(response, undefined, 2));
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
     });
