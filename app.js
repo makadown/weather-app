97131e292bbeee20672395a9378b3ccc0a314b35
Mario Serrano <maseflores79@gmail.com>
2018-06-27 21:54:52 -0700

    Validando errores de callbacks

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 /*jshint esversion: 6 */
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs.options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    }).help()
+    .alias('help', 'h')
+    .argv;
+
+// 1301 Lombard street
+// console.log(argv);
+const encodedAddress = encodeURIComponent(argv.address);
+const endPoint = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyDHht-qr0W7TYcKM8f-trqJGSf9q8fpm8g`;
 
 request({
-        url: 'https://maps.googleapis.com/maps/api/geocode/json?address=Rio%20nazas%203042%20nuevo%20mexicali&key=AIzaSyDHht-qr0W7TYcKM8f-trqJGSf9q8fpm8g',
+        url: endPoint,
         json: true
     },
     (error, response, body) => {
-        // console.log(JSON.stringify(body, undefined, 2));
-        // console.log(JSON.stringify(response, undefined, 2));
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
+
+        if (error) {
+            console.log('Unable to connect to google service');
+        } else if (body.status === 'ZERO_RESULTS') {
+            console.log('Unable to find that addresss');
+        } else if (body.status === 'OK') {
+
+            // console.log(JSON.stringify(body, undefined, 2));
+            // console.log(JSON.stringify(response, undefined, 2));
+            console.log(`Address: ${body.results[0].formatted_address}`);
+            console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
+            console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
+        }
     });
