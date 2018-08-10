03677352a0e9ad00f7eeab8bd33a80d28e4575b9
Mario Serrano <maseflores79@gmail.com>
2018-08-10 11:57:43 -0700

    metiendo servicio de forecast

--- app.js
+++ app.js
@@ -20,5 +20,25 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
         console.log(errorMessage);
     } else {
         console.log(JSON.stringify(results, undefined, 2));
+        const latitude = results.latitude;
+        const longitude = results.longitude;
+        const endPoint = `https://api.darksky.net/forecast/e0fdbc783456e99e64fe1c2e6c85b6b1/${latitude},${longitude}`;
+        request({
+                url: endPoint,
+                json: true
+            },
+            (error, response, body) => {
+                if (error) {
+                    console.log('Unable to connect to forecast service');
+                } else if (response.statusCode == 400 || response.statusCode == 404) {
+                    console.log('Unable to fetch weather');
+                } else if (response.statusCode == 200) {
+                    console.log(body.currently);
+                }
+            });
     }
 });
+
+
+
+// https://api.darksky.net/forecast/e0fdbc783456e99e64fe1c2e6c85b6b1/37.8267,-122.4233
